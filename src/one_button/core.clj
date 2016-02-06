e5baeb21d346766471ce1b674e0bce0b51735e20
Louis Pilfold <louis@lpil.uk>
2016-02-06 17:53:04 +0000

    Climb the tree

--- src/one_button/core.clj
+++ src/one_button/core.clj
@@ -5,10 +5,12 @@
 (def frames-per-tick 30)
 
 (def tree
-  {:one { }
-   :two { }
-   :three { }
-   })
+  {:one   {:one-one {}
+           :one-two {}}
+   :two   {:two-one {}
+           :two-two {}}
+   :three {:three-one {}
+           :three-two {}}})
 
 ; (def width  810)
 ; (def height 540)
@@ -16,20 +18,47 @@
 (def height 720)
 
 (defn inc-tick [state]
-  (let [size (count (keys tree))
-        max-tick (+ 1 size)
-        tick (-> state :tick inc (mod max-tick))]
+  (let [max-tick (count (:choices state))
+        tick     (-> state :tick inc (mod max-tick))]
     (assoc state :tick tick)))
+
+(defn current-choice [state]
+    (->> (:tick state) (nth (:choices state))))
 
 (defn setup []
   {:path []
    :tick 0
-   :choices (keys tree)})
+   :choices (conj (keys tree) :back)})
+
+(defn climb-tree [state]
+  (let [picked      (current-choice state)
+        new-path    (conj (:path state) picked)
+        next-level  (get-in tree new-path)
+        new-choices (keys next-level)
+        with-back   (conj new-choices :back)]
+    (assoc state
+           :path new-path
+           :choices with-back
+           :tick 0)))
+
+(defn key-pressed [state event]
+  (if (= :back (current-choice state))
+    state ; TODO: go back
+    (climb-tree state)
+    ))
+
+(key-pressed (setup) 1)
+
+(def round (assoc (setup) :tick 1))
+(key-pressed round 1)
+(get-in tree [:one])
 
 (defn step [state]
   (if (= (mod (q/frame-count) frames-per-tick) 0)
     (inc-tick state)
     state))
+
+(current-choice (setup))
 
 (defn draw [state]
   (q/background 0)
@@ -38,16 +67,17 @@
   (q/text-align :center :center)
   (q/translate (* 0.5 (q/width)) (* 0.5 (q/height)))
 
-  (let [choices (conj (:choices state) :back)
-        display (->> choices (map name) (clojure.string/join " "))
-        current (->> (:tick state) (nth choices))]
+  (let [display (->> (:choices state) (map name) (clojure.string/join " "))
+        current (current-choice state)]
+
+    (q/text-size 20)
+    (q/text (str state) 0 -200)
 
     (q/text-size 100)
     (q/text (name current) 0 0)
     (q/text-size 30)
     (q/text display 0 200)
-    )
-  )
+    ))
 
 (defn run []
   (q/defsketch example
@@ -56,6 +86,7 @@
     :settings #(q/smooth 2)
     :setup setup
     :update step
+    :key-pressed key-pressed
     :draw draw
     :size [800 800]
     ))
